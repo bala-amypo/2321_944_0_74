1e37408e6a18bec90e74d00205777f48c3a53ff4
bala-amypo <bala@amypo.in>
2025-12-18 05:17:54 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -1,56 +1,35 @@
 package com.example.demo.entity;
 
-import java.time.LocalDate;
 
 
-public class StudentEntity {
+public class Studententity {
+   
     private String name;
-    @id
-    private Long id;
-    @column(name=unique)
+    private int id;
     private String email;
-    private String password;
-    private String role;
-    public StudentEntity(String name, int id, LocalDate dob, String email, float cgpa) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.role=role;
-    }
-
-    public StudentEntity(){
-
-    }
     public String getName() {
         return name;
     }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public Long getId() {
+    public int getId() {
         return id;
-    }
-    
-    public void setId(Long id) {
-        this.id = id;
     }
     public String getEmail() {
         return email;
     }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
     public void setEmail(String email) {
         this.email = email;
     }
-    public String getRole() {
-        return role;
+    public Studententity(String name, int id, String email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
     }
-    public void setRole(String role) {
-        this.role = role;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
+    public Studententity() {
     }
 }
